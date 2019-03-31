a064796af52d483570664af140fc1249f90d8541
Shania Mahmud <smahmud2@binghamton.edu>
2019-03-30 22:00:07 -0400

    done

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
-words=["As","Bs","Cs", "Ds", "Es", "Fs", "Gs", "Hs", "Is", "Js", "Ks",
+words=["Two","Three","Four","Five","Six","Seven","Eight","Nine","As","Bs","Cs", "Ds", "Es", "Fs", "Gs", "Hs", "Is", "Js", "Ks",
 "Ls", "Ms", "Ns","Os","Ps","Qs","Rs","Ss","Ts", "Us","Vs","Ws", "Xs", "Ys", "Zs"] 
-wording=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+wording=["2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 print(len(words));
 print(len(wording));
 for i in range(len(words)):
-	print("else if (trans.compare("+wording[i]+")==0)){");
-	print("begin->add"+words[i]+"(end);}")
-	
+	print("else if (sym=='"+wording[i]+"'){");
+	print("      for x: curr->vecs"+words[i]+"){");
+	print("            path.push_back(make_pair(x, left.substr(1, left.length())))");
+	print("			}")
+	print("}")
--- mahmud_p1.cpp
+++ mahmud_p1.cpp
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]){
 				else if(trans.compare("6")==0){
 					begin->addSix(end);}
 				else if(trans.compare("7")==0){
-					begins->addSeven(end);}
+					begin->addSeven(end);}
 				else if(trans.compare("8")==0){
 					begin->addEight(end);}
 				else if(trans.compare("9")==0){
@@ -170,7 +170,7 @@ int main(int argc, char* argv[]){
 				begin->addSix(end);
 				}
 				else if(trans.compare("7")==0){
-				begins->addSeven(end);
+				begin->addSeven(end);
 				}
 				else if(trans.compare("8")==0){
 				begin->addEight(end);
@@ -234,32 +234,172 @@ int main(int argc, char* argv[]){
 			else if(begin==NULL && end!=NULL){
 				begin=new Node(state1, false, false);
 				lis.push_back(begin);
-
-
-
 			}
 			else if(begin==NULL && end==NULL){
 				if (state1!=state2){
-				begin=new Node(state1, false, false);
-				end=new Node(state2, false, false);
-				lis.push_back(begin);
-				lis.push_back(end);
-				if(trans==1){
+					begin=new Node(state1, false, false);
+					end=new Node(state2, false, false);
+					lis.push_back(begin);
+					lis.push_back(end);
+					/*if(trans==1){
 						begin->addOne(end);
-					}
-				else{
+					}else{
 						begin->addZero(end);
-				}
+					}*/
+					if(trans.compare("1")==0){
+					begin->addOne(end);}
+					else if(trans.compare("0")==0){
+					begin->addZero(end);}
+					else if(trans.compare("2")==0){
+					begin->addTwo(end);}
+					else if(trans.compare("3")==0){
+					begin->addThree(end);}
+					else if(trans.compare("4")==0){
+					begin->addFour(end);}
+					else if(trans.compare("5")==0){
+					begin->addFive(end);}
+					else if(trans.compare("6")==0){
+					begin->addSix(end);}
+					else if(trans.compare("7")==0){
+					begin->addSeven(end);}
+					else if(trans.compare("8")==0){
+					begin->addEight(end);}
+					else if(trans.compare("9")==0){
+					begin->addNine(end);}
+					else if (trans.compare("a")==0){
+					begin->addAs(end);}
+					else if (trans.compare("b")==0){
+					begin->addBs(end);}
+					else if (trans.compare("c")==0){
+					begin->addCs(end);}
+					else if (trans.compare("d")==0){
+					begin->addDs(end);}
+					else if (trans.compare("e")==0){
+					begin->addEs(end);}
+					else if (trans.compare("f")==0){
+					begin->addFs(end);}
+					else if (trans.compare("g")==0){
+					begin->addGs(end);}
+					else if (trans.compare("h")==0){
+					begin->addHs(end);}
+					else if (trans.compare("i")==0){
+					begin->addIs(end);}
+					else if (trans.compare("j")==0){
+					begin->addJs(end);}
+					else if (trans.compare("k")==0){
+					begin->addKs(end);}
+					else if (trans.compare("l")==0){
+					begin->addLs(end);}
+					else if (trans.compare("m")==0){
+					begin->addMs(end);}
+					else if (trans.compare("n")==0){
+					begin->addNs(end);}
+					else if (trans.compare("o")==0){
+					begin->addOs(end);}
+					else if (trans.compare("p")==0){
+					begin->addPs(end);}
+					else if (trans.compare("q")==0){
+					begin->addQs(end);}
+					else if (trans.compare("r")==0){
+					begin->addRs(end);}
+					else if (trans.compare("s")==0){
+					begin->addSs(end);}
+					else if (trans.compare("t")==0){
+					begin->addTs(end);}
+					else if (trans.compare("u")==0){
+					begin->addUs(end);}
+					else if (trans.compare("v")==0){
+					begin->addVs(end);}
+					else if (trans.compare("w")==0){
+					begin->addWs(end);}
+					else if (trans.compare("x")==0){
+					begin->addXs(end);}
+					else if (trans.compare("y")==0){
+					begin->addYs(end);}
+					else if (trans.compare("z")==0){
+					begin->addZs(end);}
 				}
 				else{	
 					begin=new Node(state1, false, false);
 					lis.push_back(begin);
-					if (trans==1){
+					/*if (trans==1){
 							begin->addOne(begin);
 						}
 					else{
 						begin->addZero(begin);
-					}
+					}*/
+					if(trans.compare("1")==0){
+					begin->addOne(end);}
+				else if(trans.compare("0")==0){
+					begin->addZero(end);}
+				else if(trans.compare("2")==0){
+					begin->addTwo(end);}
+				else if(trans.compare("3")==0){
+					begin->addThree(end);}
+				else if(trans.compare("4")==0){
+					begin->addFour(end);}
+				else if(trans.compare("5")==0){
+					begin->addFive(end);}
+				else if(trans.compare("6")==0){
+					begin->addSix(end);}
+				else if(trans.compare("7")==0){
+					begin->addSeven(end);}
+				else if(trans.compare("8")==0){
+					begin->addEight(end);}
+				else if(trans.compare("9")==0){
+					begin->addNine(end);}
+				else if (trans.compare("a")==0){
+					begin->addAs(end);}
+				else if (trans.compare("b")==0){
+					begin->addBs(end);}
+				else if (trans.compare("c")==0){
+					begin->addCs(end);}
+				else if (trans.compare("d")==0){
+					begin->addDs(end);}
+				else if (trans.compare("e")==0){
+					begin->addEs(end);}
+				else if (trans.compare("f")==0){
+					begin->addFs(end);}
+				else if (trans.compare("g")==0){
+					begin->addGs(end);}
+				else if (trans.compare("h")==0){
+					begin->addHs(end);}
+				else if (trans.compare("i")==0){
+					begin->addIs(end);}
+				else if (trans.compare("j")==0){
+					begin->addJs(end);}
+				else if (trans.compare("k")==0){
+					begin->addKs(end);}
+				else if (trans.compare("l")==0){
+					begin->addLs(end);}
+				else if (trans.compare("m")==0){
+					begin->addMs(end);}
+				else if (trans.compare("n")==0){
+					begin->addNs(end);}
+				else if (trans.compare("o")==0){
+					begin->addOs(end);}
+				else if (trans.compare("p")==0){
+					begin->addPs(end);}
+				else if (trans.compare("q")==0){
+					begin->addQs(end);}
+				else if (trans.compare("r")==0){
+					begin->addRs(end);}
+				else if (trans.compare("s")==0){
+					begin->addSs(end);}
+				else if (trans.compare("t")==0){
+					begin->addTs(end);}
+				else if (trans.compare("u")==0){
+					begin->addUs(end);}
+				else if (trans.compare("v")==0){
+					begin->addVs(end);}
+				else if (trans.compare("w")==0){
+					begin->addWs(end);}
+				else if (trans.compare("x")==0){
+					begin->addXs(end);}
+				else if (trans.compare("y")==0){
+					begin->addYs(end);}
+				else if (trans.compare("z")==0){
+					begin->addZs(end);}
 				}
 			}
 
@@ -268,7 +408,9 @@ int main(int argc, char* argv[]){
 
 	}
 	string arg=argv[2];
+	//cout << argv[2] << endl;
 	vector<pair<Node*, string>> path={};
+	//cout << "hello" << endl;
 	path.push_back(make_pair(starter, arg));
 	string left;
 	int symbol;
@@ -279,17 +421,187 @@ int main(int argc, char* argv[]){
 		left=path[i].second;
 		if (left.size()!=0){
 				sym=left[0];
-				symbol=sym-'0';
-				if (symbol==0){
+				//symbol=sym-'0';
+				if (sym=='0'){
 					for (auto x: curr->zeros){
 						path.push_back(make_pair(x, left.substr(1,left.length())));
 					}
 				}
-				else{
+				else if(sym=='1'){
 					for (auto x: curr->ones){
 						path.push_back(make_pair(x, left.substr(1, left.length())));
 					}
 				}
+				else if (sym=='2'){
+      					for (auto x: curr->vecsTwo){
+            					path.push_back(make_pair(x, left.substr(1, left.length())));
+					}
+				}
+				else if (sym=='3'){
+      					for (auto x: curr->vecsThree){
+            					path.push_back(make_pair(x, left.substr(1, left.length())));
+					}
+				}
+				else if (sym=='4'){
+      					for (auto x: curr->vecsFour){
+            					path.push_back(make_pair(x, left.substr(1, left.length())));
+					}
+				}
+				else if (sym=='5'){
+      					for (auto x: curr->vecsFive){
+            					path.push_back(make_pair(x, left.substr(1, left.length())));
+					}
+				}
+				else if (sym=='6'){
+      					for (auto x: curr->vecsSix){
+            					path.push_back(make_pair(x, left.substr(1, left.length())));
+					}
+				}
+				else if (sym=='7'){
+      					for (auto x: curr->vecsSeven){
+            					path.push_back(make_pair(x, left.substr(1, left.length())));
+					}
+				}
+				else if (sym=='8'){
+      					for (auto x: curr->vecsEight){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='9'){
+      for (auto x: curr->vecsNine){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='a'){
+      for (auto x: curr->vecsAs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='b'){
+      for (auto x: curr->vecsBs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='c'){
+      for (auto x: curr->vecsCs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='d'){
+      for (auto x: curr->vecsDs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='e'){
+      for (auto x: curr->vecsEs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='f'){
+      for (auto x: curr->vecsFs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='g'){
+      for (auto x: curr->vecsGs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='h'){
+      for (auto x: curr->vecsHs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='i'){
+      for (auto x: curr->vecsIs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='j'){
+      for (auto x: curr->vecsJs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='k'){
+      for (auto x: curr->vecsKs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='l'){
+      for (auto x: curr->vecsLs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='m'){
+      for (auto x: curr->vecsMs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='n'){
+      for (auto x: curr->vecsNs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='o'){
+      for (auto x: curr->vecsOs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='p'){
+      for (auto x: curr->vecsPs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='q'){
+      for (auto x: curr->vecsQs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='r'){
+      for (auto x: curr->vecsRs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='s'){
+      for (auto x: curr->vecsSs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='t'){
+      for (auto x: curr->vecsTs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='u'){
+      for (auto x: curr->vecsUs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='v'){
+      for (auto x: curr->vecsVs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='w'){
+      for (auto x: curr->vecsWs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='x'){
+      for (auto x: curr->vecsXs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='y'){
+      for (auto x: curr->vecsYs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
+else if (sym=='z'){
+      for (auto x: curr->vecsZs){
+            path.push_back(make_pair(x, left.substr(1, left.length())));
+			}
+}
 			}
 		}
 	bool acc=false;
